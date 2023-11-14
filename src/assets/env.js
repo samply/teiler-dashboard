45ca734fc9b240db6447c3825a47f60df0426e05
juarez <d.juarez@dkfz-heidelberg.de>
2023-11-14 19:51:34 +0100

    Added: Central Keycloak configuration based on groups

--- src/assets/env.js
+++ src/assets/env.js
@@ -5,9 +5,9 @@
   window["env"]["teiler"]["config"] = {
     "DEFAULT_LANGUAGE": "DE",
     "TEILER_BACKEND_URL": "http://localhost:8085",
-    "KEYCLOAK_URL": "http://localhost:8380/login",
-    "KEYCLOAK_REALM": "teiler",
-    "KEYCLOAK_CLIENT_ID": "teiler",
+    "KEYCLOAK_URL": "https://login.verbis.dkfz.de",
+    "KEYCLOAK_REALM": "test-realm-01",
+    "KEYCLOAK_CLIENT_ID": "bridgehead-test",
     "TEILER_ADMIN_NAME": "Max Mustermann",
     "TEILER_ADMIN_EMAIL": "max.mustermann@teiler-example.com",
     "TEILER_ADMIN_PHONE": "+49 123 456789",
@@ -17,7 +17,9 @@
     "TEILER_DASHBOARD_HTTP_RELATIVE_PATH": "",
     "TEILER_ORCHESTRATOR_HTTP_RELATIVE_PATH": "",
     "REPORTER_DEFAULT_TEMPLATE_ID": "ccp-qb",
-    "EXPORTER_DEFAULT_TEMPLATE_ID": "ccp"
+    "EXPORTER_DEFAULT_TEMPLATE_ID": "ccp",
+    "TEILER_USER": "bridgehead-test",
+    "TEILER_ADMIN": "bridgehead-test-admin"
   };
 
 })(this);
