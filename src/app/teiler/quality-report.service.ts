e2edc5c319262ee8f9a309f90a4f6f254274ba25
juarez <d.juarez@dkfz-heidelberg.de>
2023-12-13 18:51:01 +0100

    Changed: Fix messages xlf

--- src/app/teiler/quality-report.service.ts
+++ src/app/teiler/quality-report.service.ts
@@ -16,7 +16,7 @@ export interface reportLog {
 })
 export class QualityReportService extends EmbeddedTeilerApp {
 
-  description: string = $localize`Nutzen Sie den Reporter, um detaillierte Berichte sowie spezifizierte Qualitätsreports zu erstellen`;
+  description: string = $localize`Nutzen Sie den Reporter, um detaillierte Berichte sowie spezifizierte Qualitätsreports zu erstellen.`;
   iconClass: string = "bi bi-file-earmark-excel-fill";
   iconSourceUrl: string | undefined = undefined;
   title: string = $localize`Reporter`;
