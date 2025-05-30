3382c4cd1132815fee7ff797742105db3319d22c
Thomas Köhler <thomas.koehler@dkfz-heidelberg.de>
2025-05-30 12:14:24 +0200

    subscribe to local BehaviorSubject of configVariables instead of subscribing to the http.get method

--- src/app/teiler/dashboard-config.service.ts
+++ src/app/teiler/dashboard-config.service.ts
@@ -1,8 +1,8 @@
-import {Injectable, OnInit} from '@angular/core';
+import {Injectable} from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {getLocale} from "../route/route-utils";
-import {Observable, of} from "rxjs";
+import {BehaviorSubject, Observable} from "rxjs";
 
 export interface ConfigVariables {
   BACKGROUND_IMAGE_URL?: string;
@@ -21,16 +21,20 @@ export interface ConfigVariables {
   providedIn: 'root'
 })
 export class DashboardConfigService {
-  private config: Observable<ConfigVariables> = of({})
+  private configSubject = new BehaviorSubject<ConfigVariables>({} as ConfigVariables);
+  public $config: Observable<ConfigVariables> = this.configSubject.asObservable();
+
   locale = "";
   constructor(private httpClient: HttpClient) {
     this.locale = getLocale();
     const headers = new HttpHeaders();
     headers.set('Accept', 'application/xml');
-    this.config = this.httpClient.get(environment.config.TEILER_BACKEND_URL + '/variables/' + this.locale, {headers, responseType: 'json'})
+    this.httpClient.get(environment.config.TEILER_BACKEND_URL + '/variables/' + this.locale, {headers, responseType: 'json'}).subscribe((config) => {
+      this.configSubject.next(config);
+    })
   }
 
   public getConfig(): Observable<ConfigVariables> {
-    return this.config
+    return this.$config
   }
 }
